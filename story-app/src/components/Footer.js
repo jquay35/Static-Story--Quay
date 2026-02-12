dbe1531567919d27117dfa70b694b092cf903fe9
Jordan Quay <jordan.quay518@gmail.com>
2026-02-12 16:05:46 -0500

    Connect Header, Content, and Footer in App

---
+++ story-app/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "20px", textAlign: "center" }}>
+      <p><strong>Moral:</strong> Slow and steady wins the race.</p>
+    </footer>
+  );
+}
